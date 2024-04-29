16deae9f6900e2966f3b331d6883d9c48a582197
SoosyPoo <162006443+SoosyPoo@users.noreply.github.com>
2024-04-29 12:25:33 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//function encryptedLetter (letter, shift)
+{
+ // const index = alphabet.indexOf(letter.toLowerCase());
+ // const newIndex = (index + shift) % alphabet.length;
+ //return alphabet[newIndex];
+}
+//function encryptedMessage (word, shift)
+{
+ // let encryptedMessage = "";
+ // for (let i = 0; i < word.length; i++)
+  {
+  //  encryptedMessage += encryptedLetter(word[i], shift);
+  }
+ // return encryptedMessage;
+}
+// function decryptMessage (letter, shift)
+ {
+  // const index = alphabet.indexOf(letter.toLowerCase());
+  // const newIndex = (index - shift + alphabet.length) % alphabet.length;
+  // return alphabet[newIndex];
+ }
+//function decryptMessage (word, shift)
+{
+ //let decryptedMessage = "";
+ // for (let i = 0; i < word.length; i++)
+  {
+  //  decryptedMessage += decryptLetter(word[i], shift);
+  }
+ // return decryptedMessage;
+}
